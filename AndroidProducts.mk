8c950efb184b15b79140dd04d42b6664013266d4
Rasenkai <rasenkai99@gmail.com>
2021-06-30 06:40:11 +0000

    sweet: adapt for fluid
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/fluid_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    fluid_sweet-user \
+    fluid_sweet-userdebug \
+    fluid_sweet-eng
--- fluid_sweet.mk
+++ fluid_sweet.mk
@@ -11,11 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common fluid stuff.
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := fluid_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Fluid
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.fluid.maintainer=Rasenkai \
+  ro.fluid.cpu=SD732G
+
+# GApps
+IS_PHONE := true
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
